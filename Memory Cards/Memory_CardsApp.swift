5057d4e704ed447734f02d1bc9532c93a4dc503c
Konstantin Loginov <loginov.k@gmail.com>
2021-08-27 11:53:41 +0200

    Initial Commit

---
+++ Memory Cards/Memory_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_CardsApp.swift
+//  Memory Cards
+//
+//  Created by Konstantin Loginov on 27.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct Memory_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
