0b5e0b66b307cbb58353228c18b920656d654be9
Konstantin Loginov <loginov.k@gmail.com>
2021-09-12 15:35:43 +0200

    Updating the playground

--- Memory Cards/TestingCode.playground/Contents.swift
+++ Memory Cards/TestingCode.playground/Contents.swift
@@ -130,13 +130,20 @@ PlaygroundPage.current.setLiveView(CircleFigure())
 
 
 
+//// Structs are immutable:
+//struct Zoo {
+//    // Not that it's "var" here, not "let"
+//    var animals: [String] = ["Zebra", "Lion", "Seal"]
+//}
+//
+//Zoo().animals.append("Konstantin") // <-- failed to compile!
 
-
-
-
-
-
-
+////Class are mutatable:
+//class Zoo {
+//    var animals: [String] = ["Zebra", "Lion", "Seal"]
+//}
+//
+//Zoo().animals.append("Konstantin") // <-- works like a charm!
 
 
 
@@ -152,13 +159,59 @@ PlaygroundPage.current.setLiveView(CircleFigure())
 //
 //// Array's implementation of append knows *nothing* about the argument.
 //// Element - is just a placeholder (not a known class or struct)
-//
+
+
+
 //var a = Array<Int>()
 //a.append(5)
 //a.append(7)
 //
 //// When someone uses Array, that's when Element get determined (to Int in this case)
-//
+
+
+
+
 //// Also the same struct or class might have multiple generic types defined:
 //// struct Card<Content, Regulation> { .... }
 //
+
+
+
+//
+//protocol SuperHeroProtocol {
+//    var skillPoints: Int { get set }
+//}
+//
+//class HeroConverter<Hero: SuperHeroProtocol> {
+//    func exchangeSkills( hero1: inout Hero, hero2: inout Hero) {
+//        let oldValueHero1 = hero1.skillPoints
+//        let oldValueHero2 = hero2.skillPoints
+//        hero1.skillPoints = oldValueHero2
+//        hero2.skillPoints = oldValueHero1
+//    }
+//}
+//
+
+
+
+////Extensions
+//
+//extension String {
+//    func suffledAndReversed() {
+//        return self.shuffled().reversed()
+//    }
+//}
+//
+//extension Collection where Indices.Iterator.Element == Index {
+//    subscript (safe index: Index) -> Iterator.Element? {
+//        return indices.contains(index) ? self[index] : nil
+//    }
+//}
+
+
+
+extension SomeType: SomeProtocol, AnotherProtocol {
+    // new functionality to add to SomeType goes here
+    ...
+    // implementation of protocol requirements goes here
+}
